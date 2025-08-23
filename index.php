dffbc96381fb65928979916b671c5d02fb01e368
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-08-22 22:01:05 -0300

    conexao

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+session_start();
+require_once 'conexao.php';
+
+// Verifica se o usuário já está logado
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    // Verifica se o usuário existe
+    $sql ="SELECT * FROM usuario WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($usuario && password_verify($senha, $usuario['senha'])) {
+        // Login bem-sucedido, define variáveis de sessão
+        $_SESSION['usuario'] = $usuario['nome'];
+        $_SESSION['perfil'] = $usuario['id_perfil'];
+        $_SESSION['id_usuario'] = $usuario['id_usuario'];
+
+        // Veriifica se a senha é temporária
+        if ($usuario['senha_temporaria']) {
+            // Redireciona para a página de alteração de senha
+            header('Location: alterar_senha.php');
+            exit();
+    } else {
+        // Redireciona para a pagina principal
+        header('Location: principal.php');
+        exit();
+    }
+}else {
+    // Login invalido
+    echo "<script>alert('E-mail ou senha inválidos.');window.location.href='login.php';</script>";
+}
+}   
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
